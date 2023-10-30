0c307ae56963660294c339fdd85df9dd886e850d
quentin l'arbalestrier <quentin.larbalestrier@gmail.com>
2023-10-30 15:57:15 +0100

    intégration editButton + appel api page User

--- frontend/src/pages/user/user.jsx
+++ frontend/src/pages/user/user.jsx
@@ -1,13 +1,39 @@
 import Button from '../../components/button/button'
 import Account from '../../components/account/account';
+import EditButton from '../../components/editButton/editButton';
+
 import accountInformation from '../../data/accountInformation/accountInformation';
 
+import { useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { setGetProfile } from "../../redux/reducers/profileSlice"
+
 function User() {
+    const token = useSelector(state => state.userAuth.token)
+    const profile = useSelector((state) => state.profile)
+    const dispatch = useDispatch()
+    useEffect(() => {
+        const fetchDataUser = async () => {
+            try {
+                const response = await fetch("http://localhost:3001/api/v1/user/profile", {
+                method: "POST",
+                    headers: {
+                        "Authorization": `Bearer ${token}`
+                    }
+                })
+                const data = await response.json()
+                dispatch(setGetProfile({ data }))
+            } catch (err) {
+                console.log(err)
+            }
+        }
+        fetchDataUser()
+    }, [token])
     return (
     <div className="main  bg-dark">
         <div className="header">
-            <h1>Welcome back<br />Tony Jarvis!</h1>
-            <Button
+            <h1>Welcome back<br />{profile.firstName + " " + profile.lastName + "!"}</h1>
+            <EditButton
             className={'edit-button'}
             name={'Edit Name'}
             />
